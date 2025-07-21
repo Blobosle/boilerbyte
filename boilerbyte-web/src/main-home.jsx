ff3a2cedf1f38b265fc07a2f1c916afd4c0976a7
Benjamin Lobos <blobosle@purdue.edu>
2025-07-20 21:51:56 -0400

    Add: vercel analytics

--- boilerbyte-web/src/main-home.jsx
+++ boilerbyte-web/src/main-home.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import MainHero from "./components/MainHero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
+        <Analytics />
         <div className="bg-gradient-to-r from-stone-900 to-stone-950 min-h-screen">
             <Navbar />
             <MainHero />
