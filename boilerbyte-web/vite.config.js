10c16d357f0f9324b23fa5f70c812d264ad590c3
Benjamin Lobos <blobosle@purdue.edu>
2025-07-20 20:07:20 -0400

    Add: pathing implemented

--- boilerbyte-web/vite.config.js
+++ boilerbyte-web/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                man: resolve(__dirname, 'man/index.html'),
+                courses: resolve(__dirname, 'courses/index.html'),
+                cs240: resolve(__dirname, 'courses/cs240/index.html'),
+                cs250: resolve(__dirname, 'courses/cs250/index.html'),
+                cs251: resolve(__dirname, 'courses/cs251/index.html'),
+            }
+        }
+    }
 })
