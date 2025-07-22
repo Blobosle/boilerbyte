e5ceec4ac8ea6e728109460cbb0a66057a731d73
Benjamin Lobos <blobosle@purdue.edu>
2025-07-22 17:27:46 -0400

    Add: vercel insights

--- boilerbyte-web/src/main-home.jsx
+++ boilerbyte-web/src/main-home.jsx
@@ -4,10 +4,13 @@ import MainHero from "./components/MainHero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Analytics />
+        <SpeedInsights/>
+
         <div className="bg-gradient-to-r from-stone-900 to-stone-950 min-h-screen">
             <Navbar />
             <MainHero />
